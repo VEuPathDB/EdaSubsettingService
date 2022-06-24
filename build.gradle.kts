fe7365eabd2688f2645f2d663932c2651334fa6e
Ryan Doherty <tech@conical.org>
2022-06-24 10:29:41 -0400

    Iterate version for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ containerBuild {
     group = "org.veupathdb.service.eda"
 
     // Project Version
-    version = "1.0.0"
+    version = "3.0.0"
 
     // Project Root Package
     projectPackage = "org.veupathdb.service.eda.ss"
