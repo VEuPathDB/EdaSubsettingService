ca720cd89d64d53a509bf8ccca9e7b9d15983c84
Dan Galdi <dmgaldi@upenn.edu>
2022-09-06 14:16:32 -0400

    Added regressions tests for map-reduce subsetting

--- api-test/test-directory.sh
+++ api-test/test-directory.sh
@@ -6,14 +6,37 @@ fi
 
 CYAN="\e[36m"
 ENDCOLOR="\e[0m"
+FAILED_TESTS=()
 
-find $1 -print0 | while IFS= read -r -d '' file
+for file in "$1/*"
+do
+  echo $file
+done
+
+for file in $(find "$1")
 do
     if [[ $file == *.json ]]
     then
       echo $(printf "${CYAN}Running test for request \"$file\"${ENDCOLOR}")
       echo ""
       ./test-study.sh $file
+      if [[ $? -ne 0 ]]
+      then
+        FAILED_TESTS+=($file)
+      fi
       echo "----------------------------------"
     fi
 done
+
+if [[ ${#FAILED_TESTS[@]} -eq 0 ]]
+then
+    echo "All tests passed!"
+    exit 0
+else
+    echo "The following tests failed:"
+    for value in "${FAILED_TESTS[@]}"
+    do
+      echo "Failed test: $value"
+    done
+    exit 1
+fi
