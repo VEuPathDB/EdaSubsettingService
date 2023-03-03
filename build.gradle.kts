ce629a72243ccb8ea5425c7566218f2c0a1402bb
Dan Galdi <dmgaldi@upenn.edu>
2023-03-03 09:45:41 -0500

    Fix the regression test task

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,9 +60,11 @@ tasks.shadowJar {
 }
 
 tasks.register("regression-test") {
-  project.exec {
-    workingDir("$projectDir/api-test")
-    commandLine("./test-directory.sh", "$projectDir/api-test/requests", "output/results")
+  doLast {
+    project.exec {
+      workingDir("$projectDir/api-test")
+      commandLine("./test-directory.sh", "$projectDir/api-test/requests", "output/results")
+    }
   }
 }
 
