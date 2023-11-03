5a3207bfb7a655cfce1f9db803b4cf256bbdbcf9
Dan Galdi <dmgaldi@upenn.edu>
2023-11-03 12:18:34 -0400

    Log count request filters (#123)

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,7 +86,7 @@ repositories {
 //
 
 // versions
-val coreLib       = "6.16.0"         // Container core lib version
+val coreLib       = "6.19.1"         // Container core lib version
 val edaCommon     = "11.5.4"         // EDA Common version
 val libSubsetting = "4.10.1"         // lib-eda-subsetting version
 val fgputil       = "2.12.9-jakarta" // FgpUtil version
