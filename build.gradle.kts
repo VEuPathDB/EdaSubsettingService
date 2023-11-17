5e0cd03b5d0f4eb14ab22f77150670bece72321d
Ryan Doherty <tech@conical.org>
2023-11-17 12:51:31 -0500

    Add logging and upgrade lib-subsetting

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,7 +88,7 @@ repositories {
 // versions
 val coreLib       = "6.19.1"         // Container core lib version
 val edaCommon     = "11.5.4"         // EDA Common version
-val libSubsetting = "4.10.1"         // lib-eda-subsetting version
+val libSubsetting = "4.11.2"         // lib-eda-subsetting version
 val fgputil       = "2.12.9-jakarta" // FgpUtil version
 
 // use local EdaCommon compiled schema if project exists, else use released version;
