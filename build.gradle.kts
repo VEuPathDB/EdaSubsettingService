123b4f0114e2af2a68288bdb4f1589af644b58cc
Dan Galdi <dmgaldi@upenn.edu>
2024-03-08 13:51:36 -0500

    Upgrade to oauth version of container core (#135)
    * Upgrade to oauth version of container core
    
    * Latest dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,10 +86,10 @@ repositories {
 //
 
 // versions
-val coreLib       = "6.19.1"         // Container core lib version
-val edaCommon     = "11.7.1"         // EDA Common version
+val coreLib       = "7.0.1"         // Container core lib version
+val edaCommon     = "11.7.2"         // EDA Common version
 val libSubsetting = "4.13.10"         // lib-eda-subsetting version
-val fgputil       = "2.12.18-jakarta" // FgpUtil version
+val fgputil       = "2.13.1-jakarta" // FgpUtil version
 
 // use local EdaCommon compiled schema if project exists, else use released version;
 //    this mirrors the way we use local EdaCommon code if available
