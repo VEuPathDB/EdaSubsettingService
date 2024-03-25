af14e09ee5ca418b67998cdde643d8e1cbc1d0b7
Dan Galdi <dmgaldi@upenn.edu>
2024-03-25 15:08:19 -0400

    Use latest container core

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,7 +86,7 @@ repositories {
 //
 
 // versions
-val coreLib       = "7.0.1"         // Container core lib version
+val coreLib       = "7.0.6"         // Container core lib version
 val edaCommon     = "11.7.2"         // EDA Common version
 val libSubsetting = "4.13.11"         // lib-eda-subsetting version
 val fgputil       = "2.13.1-jakarta" // FgpUtil version
